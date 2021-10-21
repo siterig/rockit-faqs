3b293228650a6c14a3c354dcadac8af2c828d593
Matt Stone <matt@rockandscissor.com>
2021-10-21 10:10:23 +0100

    Started building JS class

---
+++ js/rockit-faqs.js
@@ -0,0 +1,12 @@
+class RockitFaqs {
+    constructor(cssCdnYes, cssCdnNo, javascriptLibrary, javascriptCdnYes, javascriptCdnNo, scNumberOfFaqs, scRandomOrderYes,  ) {
+
+        this.cssCdnYes = document.getElementById('rockit_faqs_css-cdn-yes');
+        this.cssCdnNo = document.getElementById('rockit_faqs_css-cdn-no');
+
+        this.javascriptLibrary = document.getElementById('rockit_faqs_javascript_library');
+        this.javascriptCdnYes = document.getElementById('rockit_faqs_javascript_cdn_yes');
+        this.javascriptCdnNo = document.getElementById('rockit_faqs_javascript_cdn_no');
+
+    }
+}
